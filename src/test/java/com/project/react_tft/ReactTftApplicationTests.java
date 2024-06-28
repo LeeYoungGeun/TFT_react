048ed6a92826721afc6bd4f4cd66d09bdaa4f9a4
YoungGeun <crusade6702@gmail.com>
2024-06-29 00:58:50 +0900

    first commit by master

---
+++ src/test/java/com/project/react_tft/ReactTftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.react_tft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
